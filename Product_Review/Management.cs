0bd055d11650686afa510fb5bcf184d81e6d264d
Rajajr <rajjr011@gmail.com>
2022-11-11 12:13:03 +0700

    UC-2 Retrieve Top3 Records From The List

---
+++ Product_Review/Management.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Review
+{
+    public class Management
+    {
+        public readonly DataTable datatable = new DataTable();
+
+        //creating a method
+        public void Top3Records(List<Productreview>listproductreview)
+        {
+            var recordedData = (from Productreviews in listproductreview
+                                orderby Productreviews.Rating descending
+                                select Productreviews).Take(3);
+
+            Console.WriteLine("Top Three Records : ");
+            
+            foreach(var list in recordedData)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID + " " + "Rating:-" + list.Rating + " " + 
+                                     "Review:-" + list.Review + " " + "is_Like:-" + list.is_Like);
+                Console.WriteLine("------------------------------------------------------------------------------------------------");
+            }
+        }
+
+    }
+}
--- Product_Review/Program.cs
+++ Product_Review/Program.cs
@@ -17,26 +17,30 @@ namespace Product_Review
 
                 new Productreview() { ProductID = 1, UserID = 1, Rating = 2, Review = "Good", is_Like = true },
                 new Productreview() { ProductID = 2, UserID = 2, Rating = 3, Review = "Good", is_Like = true },
-                new Productreview() { ProductID = 3, UserID = 3, Rating = 4, Review = "Good", is_Like = true },
+                new Productreview() { ProductID = 3, UserID = 3, Rating = 4.2, Review = "Good", is_Like = true },
                 new Productreview() { ProductID = 4, UserID = 4, Rating = 2, Review = "Good", is_Like = true },
-                new Productreview() { ProductID = 5, UserID = 5, Rating = 5, Review = "Good", is_Like = true },
+                new Productreview() { ProductID = 5, UserID = 5, Rating = 5.5, Review = "Good", is_Like = true },
                 new Productreview() { ProductID = 6, UserID = 1, Rating = 2, Review = "Bad", is_Like = true },
                 new Productreview() { ProductID = 1, UserID = 2, Rating = 3, Review = "nice", is_Like = true },
-                new Productreview() { ProductID = 11, UserID = 3, Rating = 4, Review = "nice", is_Like = true },
+                new Productreview() { ProductID = 11, UserID = 3, Rating = 1, Review = "nice", is_Like = true },
                 new Productreview() { ProductID = 10, UserID = 4, Rating = 2, Review = "nice", is_Like = true },
-                new Productreview() { ProductID = 12, UserID = 5, Rating = 5, Review = "nice", is_Like = true },
+                new Productreview() { ProductID = 12, UserID = 5, Rating = 3, Review = "nice", is_Like = true },
                 new Productreview() { ProductID = 13, UserID = 1, Rating = 2, Review = "nice", is_Like = true },
                 new Productreview() { ProductID = 14, UserID = 2, Rating = 3, Review = "nice", is_Like = true },
                 new Productreview() { ProductID = 15, UserID = 3, Rating = 4, Review = "nice", is_Like = true },
                 new Productreview() { ProductID = 16, UserID = 4, Rating = 2, Review = "nice", is_Like = true },
 
             };
-            Console.WriteLine("Number of Rows in the Table");
-            foreach(var list in productreviewlist)
-            {
-                Console.WriteLine("ProductID:-" +list.ProductID+ " " +"UserID:-"+list.UserID+" "+"Rating:-"+list.Rating+" "+"Review:-"+list.Review+" "+"is_Like:-"+list.is_Like);
+            //Console.WriteLine("Number of Rows in the Table");
+            //foreach(var list in productreviewlist)
+            //{
+            //    Console.WriteLine("ProductID:-" +list.ProductID+ " " +"UserID:-"+list.UserID+" "+"Rating:-"+list.Rating+" "+"Review:-"+list.Review+" "+"is_Like:-"+list.is_Like);
 
-            }
+           // }
+
+            Management management = new Management();
+            management.Top3Records(productreviewlist);
+
             Console.ReadLine();
         }
     }
